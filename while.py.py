95f6df55ad4da0f42019e86a0bfc3c41b0107a8d
Makhsuja <makhsuja18@gmail.com>
2024-10-31 19:52:15 +0300

    while.py.py

--- while.py.py
+++ while.py.py
@@ -1,11 +1,11 @@
 while True:
     number = int(input('Введите число: '))
     my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-    if number % 1 == 0:
+    if number > 0 == 0:
         print("Положительное число ")
         continue
-    elif number % -0 == 0:
+    elif number < -0 == 0:
         print("Отрицательное число")
     else:
         break
-print('Выход за границу')
+        print('Выход за границу')
